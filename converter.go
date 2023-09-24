17afa34730c39416c4851600f7a511a5c2b921be
k1LoW <k1lowxb@gmail.com>
2023-09-24 22:48:42 +0900

    Add --statistics option and default to med-by-ns-per-op

--- converter.go
+++ converter.go
@@ -15,7 +15,16 @@ type benchGroup struct {
 	n      int
 }
 
-func Convert(set parse.Set) []*report.CustomMetricSet {
+const (
+	StasticsTypeAvg                    = "avg"
+	StasticsTypeMedByN                 = "med-by-n"
+	StasticsTypeMedByNsPerOp           = "med-by-ns-per-op"
+	StasticsTypeMedByMBPerS            = "med-by-mb-per-s"
+	StasticsTypeMedByAllocedBytesPerOp = "med-by-alloced-bytes-per-op"
+	StasticsTypeMedByAllocsPerOp       = "med-by-allocs-per-op"
+)
+
+func Convert(set parse.Set, stype string) ([]*report.CustomMetricSet, error) {
 	var keys []string
 	for k := range set {
 		keys = append(keys, k)
@@ -36,76 +45,200 @@ func Convert(set parse.Set) []*report.CustomMetricSet {
 				g[b.Name] = &benchGroup{
 					key:    b.Name,
 					benchs: []*parse.Benchmark{b},
-					n:      1,
 				}
 				continue
 			}
 			bg.benchs = append(bg.benchs, b)
-			bg.n++
 		}
 		names = unique(names)
 		for _, n := range names {
 			bg := g[n]
 			name := n
-			if bg.n > 1 {
-				name = fmt.Sprintf("%s (average of %d benchmarks)", name, bg.n)
+			var (
+				metrics []*report.CustomMetric
+				err     error
+			)
+			switch stype {
+			case StasticsTypeAvg:
+				if len(bg.benchs) > 1 {
+					name = fmt.Sprintf("%s (average of %d benchmarks)", name, len(bg.benchs))
+				}
+				metrics, err = avg(bg.benchs)
+				if err != nil {
+					return nil, err
+				}
+			case StasticsTypeMedByN:
+				if len(bg.benchs) > 1 {
+					name = fmt.Sprintf("%s (median by %q of %d benchmarks)", name, "Number of iterations", len(bg.benchs))
+				}
+				metrics, err = medByN(bg.benchs)
+				if err != nil {
+					return nil, err
+				}
+			case StasticsTypeMedByNsPerOp:
+				if len(bg.benchs) > 1 {
+					name = fmt.Sprintf("%s (median by %q of %d benchmarks)", name, "Nanoseconds per iteration", len(bg.benchs))
+				}
+				metrics, err = medByNsPerOp(bg.benchs)
+				if err != nil {
+					return nil, err
+				}
+			case StasticsTypeMedByMBPerS:
+				if len(bg.benchs) > 1 {
+					name = fmt.Sprintf("%s (median by %q of %d benchmarks)", name, "MB processed per second", len(bg.benchs))
+				}
+				metrics, err = medByMBPerS(bg.benchs)
+				if err != nil {
+					return nil, err
+				}
+			case StasticsTypeMedByAllocedBytesPerOp:
+				if len(bg.benchs) > 1 {
+					name = fmt.Sprintf("%s (median by %q of %d benchmarks)", name, "Bytes allocated per iteration", len(bg.benchs))
+				}
+				metrics, err = medByAllocedBytesPerOp(bg.benchs)
+				if err != nil {
+					return nil, err
+				}
+			case StasticsTypeMedByAllocsPerOp:
+				if len(bg.benchs) > 1 {
+					name = fmt.Sprintf("%s (median by %q of %d benchmarks)", name, "Allocs per iteration", len(bg.benchs))
+				}
+				metrics, err = medByAllocsPerOp(bg.benchs)
+				if err != nil {
+					return nil, err
+				}
+			default:
+				return nil, fmt.Errorf("unknown stastics type: %s", stype)
 			}
 			cs := &report.CustomMetricSet{
-				Name: name,
-				Key:  n,
-			}
-			b := bg.benchs[0]
-			cs.Metrics = append(cs.Metrics, &report.CustomMetric{
-				Name: "Number of iterations",
-				Key:  "N",
-				Value: lo.Reduce(bg.benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
-					return agg + float64(b.N)
-				}, 0.0) / float64(bg.n),
-			})
-			if (b.Measured & parse.NsPerOp) != 0 {
-				cs.Metrics = append(cs.Metrics, &report.CustomMetric{
-					Name: "Nanoseconds per iteration",
-					Key:  "NsPerOp",
-					Value: lo.Reduce(bg.benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
-						return agg + float64(b.NsPerOp)
-					}, 0.0) / float64(bg.n),
-					Unit: " ns/op",
-				})
-			}
-			if (b.Measured & parse.MBPerS) != 0 {
-				cs.Metrics = append(cs.Metrics, &report.CustomMetric{
-					Name: "MB processed per second",
-					Key:  "MBPerS",
-					Value: lo.Reduce(bg.benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
-						return agg + float64(b.MBPerS)
-					}, 0.0) / float64(bg.n),
-					Unit: " MB/s",
-				})
-			}
-			if (b.Measured & parse.AllocedBytesPerOp) != 0 {
-				cs.Metrics = append(cs.Metrics, &report.CustomMetric{
-					Name: "Bytes allocated per iteration",
-					Key:  "AllocedBytesPerOp",
-					Value: lo.Reduce(bg.benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
-						return agg + float64(b.AllocedBytesPerOp)
-					}, 0.0) / float64(bg.n),
-					Unit: " B/op",
-				})
-			}
-			if (b.Measured & parse.AllocsPerOp) != 0 {
-				cs.Metrics = append(cs.Metrics, &report.CustomMetric{
-					Name: "Allocs per iteration",
-					Key:  "AllocsPerOp",
-					Value: lo.Reduce(bg.benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
-						return agg + float64(b.AllocsPerOp)
-					}, 0.0) / float64(bg.n),
-					Unit: " allocs/op",
-				})
+				Name:    name,
+				Key:     n,
+				Metrics: metrics,
 			}
 			cset = append(cset, cs)
 		}
 	}
-	return cset
+	return cset, nil
+}
+
+func avg(benchs []*parse.Benchmark) ([]*report.CustomMetric, error) {
+	metrics := []*report.CustomMetric{}
+	b := benchs[0]
+	n := len(benchs)
+	metrics = append(metrics, &report.CustomMetric{
+		Name: "Number of iterations",
+		Key:  "N",
+		Value: lo.Reduce(benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
+			return agg + float64(b.N)
+		}, 0.0) / float64(n),
+	})
+	if (b.Measured & parse.NsPerOp) != 0 {
+		metrics = append(metrics, &report.CustomMetric{
+			Name: "Nanoseconds per iteration",
+			Key:  "NsPerOp",
+			Value: lo.Reduce(benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
+				return agg + float64(b.NsPerOp)
+			}, 0.0) / float64(n),
+			Unit: " ns/op",
+		})
+	}
+	if (b.Measured & parse.MBPerS) != 0 {
+		metrics = append(metrics, &report.CustomMetric{
+			Name: "MB processed per second",
+			Key:  "MBPerS",
+			Value: lo.Reduce(benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
+				return agg + float64(b.MBPerS)
+			}, 0.0) / float64(n),
+			Unit: " MB/s",
+		})
+	}
+	if (b.Measured & parse.AllocedBytesPerOp) != 0 {
+		metrics = append(metrics, &report.CustomMetric{
+			Name: "Bytes allocated per iteration",
+			Key:  "AllocedBytesPerOp",
+			Value: lo.Reduce(benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
+				return agg + float64(b.AllocedBytesPerOp)
+			}, 0.0) / float64(n),
+			Unit: " B/op",
+		})
+	}
+	if (b.Measured & parse.AllocsPerOp) != 0 {
+		metrics = append(metrics, &report.CustomMetric{
+			Name: "Allocs per iteration",
+			Key:  "AllocsPerOp",
+			Value: lo.Reduce(benchs, func(agg float64, b *parse.Benchmark, _ int) float64 {
+				return agg + float64(b.AllocsPerOp)
+			}, 0.0) / float64(n),
+			Unit: " allocs/op",
+		})
+	}
+	return metrics, nil
+}
+
+func medByN(benchs []*parse.Benchmark) ([]*report.CustomMetric, error) {
+	sort.Slice(benchs, func(i, j int) bool {
+		return benchs[i].N < benchs[j].N
+	})
+	if len(benchs)%2 == 0 {
+		return avg([]*parse.Benchmark{benchs[len(benchs)/2-1], benchs[len(benchs)/2]})
+	}
+	return avg([]*parse.Benchmark{benchs[len(benchs)/2]})
+}
+
+func medByNsPerOp(benchs []*parse.Benchmark) ([]*report.CustomMetric, error) {
+	b := benchs[0]
+	if (b.Measured & parse.NsPerOp) == 0 {
+		return nil, fmt.Errorf("benchmarks do not have NsPerOp")
+	}
+	sort.Slice(benchs, func(i, j int) bool {
+		return benchs[i].NsPerOp < benchs[j].NsPerOp
+	})
+	if len(benchs)%2 == 0 {
+		return avg([]*parse.Benchmark{benchs[len(benchs)/2-1], benchs[len(benchs)/2]})
+	}
+	return avg([]*parse.Benchmark{benchs[len(benchs)/2]})
+}
+
+func medByMBPerS(benchs []*parse.Benchmark) ([]*report.CustomMetric, error) {
+	b := benchs[0]
+	if (b.Measured & parse.MBPerS) == 0 {
+		return nil, fmt.Errorf("benchmarks do not have MBPerS")
+	}
+	sort.Slice(benchs, func(i, j int) bool {
+		return benchs[i].MBPerS < benchs[j].MBPerS
+	})
+	if len(benchs)%2 == 0 {
+		return avg([]*parse.Benchmark{benchs[len(benchs)/2-1], benchs[len(benchs)/2]})
+	}
+	return avg([]*parse.Benchmark{benchs[len(benchs)/2]})
+}
+
+func medByAllocedBytesPerOp(benchs []*parse.Benchmark) ([]*report.CustomMetric, error) {
+	b := benchs[0]
+	if (b.Measured & parse.AllocedBytesPerOp) == 0 {
+		return nil, fmt.Errorf("benchmarks do not have AllocedBytesPerOp")
+	}
+	sort.Slice(benchs, func(i, j int) bool {
+		return benchs[i].AllocedBytesPerOp < benchs[j].AllocedBytesPerOp
+	})
+	if len(benchs)%2 == 0 {
+		return avg([]*parse.Benchmark{benchs[len(benchs)/2-1], benchs[len(benchs)/2]})
+	}
+	return avg([]*parse.Benchmark{benchs[len(benchs)/2]})
+}
+
+func medByAllocsPerOp(benchs []*parse.Benchmark) ([]*report.CustomMetric, error) {
+	b := benchs[0]
+	if (b.Measured & parse.AllocsPerOp) == 0 {
+		return nil, fmt.Errorf("benchmarks do not have AllocsPerOp")
+	}
+	sort.Slice(benchs, func(i, j int) bool {
+		return benchs[i].AllocsPerOp < benchs[j].AllocsPerOp
+	})
+	if len(benchs)%2 == 0 {
+		return avg([]*parse.Benchmark{benchs[len(benchs)/2-1], benchs[len(benchs)/2]})
+	}
+	return avg([]*parse.Benchmark{benchs[len(benchs)/2]})
 }
 
 func unique(s []string) []string {
--- converter_test.go
+++ converter_test.go
@@ -12,10 +12,11 @@ import (
 
 func TestConverter(t *testing.T) {
 	tests := []struct {
-		in   parse.Set
-		want []*report.CustomMetricSet
+		in    parse.Set
+		stype string
+		want  []*report.CustomMetricSet
 	}{
-		{parse.Set{}, nil},
+		{parse.Set{}, StasticsTypeAvg, nil},
 		{
 			parse.Set{
 				"Benchmark-0": []*parse.Benchmark{
@@ -29,6 +30,7 @@ func TestConverter(t *testing.T) {
 					},
 				},
 			},
+			StasticsTypeAvg,
 			[]*report.CustomMetricSet{
 				{
 					Name: "Benchmark-0",
@@ -55,6 +57,7 @@ func TestConverter(t *testing.T) {
 					},
 				},
 			},
+			StasticsTypeAvg,
 			[]*report.CustomMetricSet{
 				{
 					Name: "Benchmark-0",
@@ -86,6 +89,7 @@ func TestConverter(t *testing.T) {
 					},
 				},
 			},
+			StasticsTypeAvg,
 			[]*report.CustomMetricSet{
 				{
 					Name: "Benchmark-0 (average of 2 benchmarks)",
@@ -99,10 +103,48 @@ func TestConverter(t *testing.T) {
 				},
 			},
 		},
+		{
+			parse.Set{
+				"Benchmark-0": []*parse.Benchmark{
+					{
+						Name:              "Benchmark-0",
+						N:                 1,
+						NsPerOp:           100,
+						AllocedBytesPerOp: 25,
+						AllocsPerOp:       50,
+						Measured:          parse.NsPerOp | parse.AllocedBytesPerOp | parse.AllocsPerOp,
+					},
+					{
+						Name:              "Benchmark-0",
+						N:                 1,
+						NsPerOp:           10,
+						AllocedBytesPerOp: 25,
+						AllocsPerOp:       100,
+						Measured:          parse.NsPerOp | parse.AllocedBytesPerOp | parse.AllocsPerOp,
+					},
+				},
+			},
+			StasticsTypeMedByNsPerOp,
+			[]*report.CustomMetricSet{
+				{
+					Name: "Benchmark-0 (median by \"Nanoseconds per iteration\" of 2 benchmarks)",
+					Key:  "Benchmark-0",
+					Metrics: []*report.CustomMetric{
+						{Name: "Number of iterations", Key: "N", Value: 1},
+						{Name: "Nanoseconds per iteration", Key: "NsPerOp", Value: 55, Unit: " ns/op"},
+						{Name: "Bytes allocated per iteration", Key: "AllocedBytesPerOp", Value: 25, Unit: " B/op"},
+						{Name: "Allocs per iteration", Key: "AllocsPerOp", Value: 75, Unit: " allocs/op"},
+					},
+				},
+			},
+		},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			got := Convert(tt.in)
+			got, err := Convert(tt.in, tt.stype)
+			if err != nil {
+				t.Fatal(err)
+			}
 			opts := []cmp.Option{
 				cmpopts.IgnoreFields(report.CustomMetricSet{}, "report"),
 			}
